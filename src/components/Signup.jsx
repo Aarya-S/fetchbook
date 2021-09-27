54b56c8bf053afe5c71286e07c7f913056ca1978
Pranav <pranavyede@gmail.com>
2021-09-27 17:45:28 +0530

    signup

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,4 +1,5 @@
 import React,{ Component } from "react";
+import '../css/signup.css';
 
 class SignUP extends Component{
     state={
@@ -6,8 +7,33 @@ class SignUP extends Component{
     }
     render(){
         return(
-            <div>
-              A sign up page  
+            <div>  
+                <div className="signup-form">
+                    <form action="/examples/actions/confirmation.php" method="post">
+		<h2>Create an Account</h2>
+		<p className="hint-text">Sign up with your social media account or email address</p>
+		<div className="social-btn text-center">
+			<a href="#" class="btn btn-primary btn-lg"><i class="fa fa-google"></i> Google</a>
+		</div>
+		<div className="or-seperator"><b>or</b></div>
+        <div className="form-group">
+        	<input type="text" class="form-control input-lg" name="username" placeholder="Username" required="required"/>
+        </div>
+		<div className="form-group">
+        	<input type="email" class="form-control input-lg" name="email" placeholder="Email Address" required="required"/>
+        </div>
+		<div className="form-group">
+            <input type="password" class="form-control input-lg" name="password" placeholder="Password" required="required"/>
+        </div>
+		<div className="form-group">
+            <input type="password" class="form-control input-lg" name="confirm_password" placeholder="Confirm Password" required="required"/>
+        </div>  
+        <div className="form-group">
+            <button type="submit" class="btn btn-success btn-lg btn-block signup-btn">Sign Up</button>
+        </div>
+    </form>
+    <div className="text-center">Already have an account? <a href="#">Login here</a></div>
+            </div>
             </div>
         )
     }
