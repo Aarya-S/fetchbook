3674eacaf0c10233ae96357c504487c49123135e
RoHiT-engi <79554502+RoHiT-engi@users.noreply.github.com>
2021-09-28 17:41:47 +0530

    seperated ImageSlider component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,6 @@
-import React, { Component, useState  } from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from 'react-icons/fa';
+import ImageSlider from "./ImageSlider";
 import '../css/Home.css';
 import BookCard from './Bookcard'
 import Navbar from './Navbar';
@@ -75,49 +75,6 @@ const RenderBooks=(state)=>{
 })}</div>
   )
 }
-
-const ImageSlider = ({ slides }) => {
-  const [current, setCurrent] = useState(0);
-  const length = slides.length;
-
-  const nextSlide = () => {
-    setCurrent(current === length - 1 ? 0 : current + 1);
-  };
-
-  const prevSlide = () => {
-    setCurrent(current === 0 ? length - 1 : current - 1);
-  };
-
-  if (!Array.isArray(slides) || slides.length <= 0) {
-    return null;
-  }
-
-  return (
-    <section className='slider'>
-      <FaArrowAltCircleLeft className='left-arrow' onClick={prevSlide} />
-      <FaArrowAltCircleRight className='right-arrow' onClick={nextSlide} />
-      {Sliderdata.map((slide, index) => {
-        return (
-          <div
-            className={index === current ? 'slide active' : 'slide'}
-            key={index}
-          >
-            {index === current && (
-              <Link to='/offers'><img src={slide.image} alt='travel image' className='image' /></Link>
-            )}
-          </div>
-        );
-      })}
-    </section>
-  );
-};
-
-
-
-
-
-
-
 
   
   export default populate;
