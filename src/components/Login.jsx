62087babd3eeb0c29e9c2f331de80ce301aa0a2b
RoHiT-engi <79554502+RoHiT-engi@users.noreply.github.com>
2021-10-01 09:48:20 +0530

    member login added

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,15 +1,19 @@
-import React,{ Component } from "react";
+import React,{ useState } from "react";
 import { Link } from "react-router-dom";
 import '../css/signup.css';
-import { signInWithGoogle } from "../firebaseconfig";
+import { signInWithGoogle,signInWithEmailnPassword,auth } from "../firebaseconfig";
 
-class SignUP extends Component{
-    render(){
-
+const SignUP =()=>{
+        const [loginid, setloginid] = useState('')
+        const [pwd, setpwd] = useState('')
+        const [logedin,setlogedin] = useState('')
+        const submit = ()=>{
+            signInWithEmailnPassword(loginid,pwd)
+            setlogedin(auth.currentUser)
+        }
         return(
             <>  
-                <div className="signup-form">
-                    <form action="/examples/actions/confirmation.php" method="post">
+        <div className="signup-form">
 		<h2>Login</h2>
 		<p className="hint-text">Sign in with your social media account or email address</p>
 		<div className="social-btn text-center">
@@ -17,21 +21,20 @@ class SignUP extends Component{
 		</div>
 		<div className="or-seperator"><b>or</b></div>
 		<div className="form-group">
-            <input type="text" class="form-control input-lg" name="Login ID" placeholder="Email ID" required="required"/>
+            <input type="text" class="form-control input-lg" value={loginid} name="Login ID" placeholder="Email ID" onChange={(e)=>{setloginid(e.target.value)}} required="required"/>
         </div>  
         <div className="form-group">
-            <input type="password" class="form-control input-lg" name="password" placeholder="password" required="required"/>
+            <input type="password" class="form-control input-lg" value={pwd} name="password" placeholder="password" onChange={(e)=>{setpwd(e.target.value)}} required="required"/>
         </div> 
         <Link to='/forgotpassword'><p>forgot password</p></Link>
-        <div className="form-group">
-            <button type="submit" class="btn btn-success btn-lg btn-block signup-btn">Sign in</button>
+        <div className="form-group"> 
+            <button onClick={submit} class="btn btn-success btn-lg btn-block signup-btn">Sign in</button>
         </div>
-    </form>
-    <div className="text-center">Dont have an account?<Link to='/signup'> <a>Sign up here</a></Link></div>
+        <div className="text-center">Dont have an account?<Link to='/signup'> <a>Sign up here</a></Link></div>
             </div>
             </>
         )
-    }
 }
+
 
 export default SignUP
