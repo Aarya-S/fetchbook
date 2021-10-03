e72e39261060dc28518c3721516d32bde62b5b52
Pranav <pranavyede@gmail.com>
2021-10-03 14:38:00 +0530

    Update Signup.jsx

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,7 +1,7 @@
 import React,{useState} from "react";
 import { Link } from "react-router-dom";
 import { signUpWithEmailAndPassword ,signInWithGoogle} from "../firebaseconfig";
-import '../css/Signup.css';
+import '../css/signup.css';
 
 
 const SignUP=()=>{
