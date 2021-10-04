e874d38e1416688865f08e5f0ddacb16c943af40
Pranav <pranavyede@gmail.com>
2021-10-04 23:53:58 +0530

    Footer links added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,6 +4,7 @@ import {Link} from 'react-router-dom'
 import logo from '../assets/favicon-32x32.png'
 import { Icon, TextField } from "@material-ui/core";  
 
+
 const Footer = () => {
 return(
     <div className="main-footer">
@@ -11,36 +12,36 @@ return(
             <div className="row">
                 {/* Column1 */}
                 <div className="col">
-                    <h4>contact us</h4>
+                    <h4>Contact Us</h4>
                     <ul className="list-unstyled">
-                        <li>+123456789</li>
-                        <li>ADDRESS</li>
-                        <li>MUMBAI XYZ</li>
+                        <li ><a href="tel:9004960511">+919004960511</a></li>
+                        <li><a href="https://mail.google.com">fetchbook@gmail.com</a></li>
+                        <li><a href="https://www.google.com/maps/place/Ramrao+Adik+Institute+of+Technology/@19.0443847,73.0257006,15z/data=!4m5!3m4!1s0x0:0x95e9ca007676b993!8m2!3d19.0443847!4d73.0257006">Nerul, Navi Mumbai, India</a></li>
                     </ul>
                 </div>
                 {/* Column2 */}
                 <div className="col">
-                    <h4>stuff</h4>
+                    <h4>Our Company</h4>
                     <ul className="list-unstyled">
-                        <li>first </li>
-                        <li>second</li>
-                        <li>third</li>
+                        <li><a>Our Team</a></li>
+                        <li><a>FAQ</a></li>
+                        <li><a>Jobs/Career</a></li>
                     </ul>
                 </div>
                 {/* Column3 */}
                 <div className="col">
-                    <h4>another column</h4>
+                    <h4>Find us on</h4>
                     <ul className="list-unstyled">
-                        <li>facebook</li>
-                        <li>insta</li>
-                        <li>twiter</li>
+                        <li><a href="https://www.facebook.com/">Facebook</a></li>
+                        <li><a href="https://www.instagram.com/">Instagram</a></li>
+                        <li><a href="https://www.whatsapp.com">Whatsapp</a></li>
                     </ul>
                 </div>
             </div>
             <hr />
             <div className="row">
                 <p className="col-sm">
-                    &copy;{new Date().getFullYear()} FETCHBOOK.COM | ALL right reserved | Terms of Service | Privacy
+                    &copy;{new Date().getFullYear()} <Link to='/'>fetchbook.com </Link>|<Link to='/About'> All rights reserved</Link> |<Link to='/About'> Terms of Service </Link>| <Link to='/About'>Privacy</Link>
                 </p>
             </div>
 
