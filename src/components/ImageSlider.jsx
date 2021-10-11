1be7512bb797ca71afd6e77cb1d1850d69533648
Aarya <aaryashelar000@gmail.com>
2021-10-11 15:12:01 +0530

    Update ImageSlider.jsx

--- src/components/ImageSlider.jsx
+++ src/components/ImageSlider.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from 'react-icons/fa';
 import { Link } from "react-router-dom";
+import { Carousel } from "react-bootstrap";
 
 const ImageSlider = ({ slides }) => {
     const [current, setCurrent] = useState(0);
@@ -35,6 +36,49 @@ const ImageSlider = ({ slides }) => {
           );
         })}
       </section>
+//   <Carousel variant="dark">
+//   <Carousel.Item>
+//     <img
+//       className="d-block w-100"
+//       src="src\assets\FetchBookBlack.svg"
+//       alt="First slide"
+//       height="300px"
+//       width="100%"
+//     />
+//     <Carousel.Caption>
+//       <h3>First slide label</h3>
+//       <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+//     </Carousel.Caption>
+//   </Carousel.Item>
+//   <Carousel.Item>
+//     <img
+//       className="d-block w-100"
+//       src="src\assets\FetchBookBlack.svg"
+//       alt="Second slide"
+//       height="300px"
+//       width="100%"
+//     />
+
+//     <Carousel.Caption>
+//       <h3>Second slide label</h3>
+//       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+//     </Carousel.Caption>
+//   </Carousel.Item>
+//   <Carousel.Item>
+//     <img
+//       className="d-block w-100"
+//       src="src\assets\FetchBookBlack.svg"
+//       alt="Third slide"
+//       height="300px"
+//       width="100%"
+//     />
+
+//     <Carousel.Caption>
+//       <h3>Third slide label</h3>
+//       <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+//     </Carousel.Caption>
+//   </Carousel.Item>
+// </Carousel>
     );
   };
 
