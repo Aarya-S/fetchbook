70a729e90d3e7423d91b3afd7c335a39ba26c9fb
RoHiT-engi <79554502+RoHiT-engi@users.noreply.github.com>
2021-11-04 08:18:24 +0530

    fixed order history card
    functionality is not added

--- src/screens/CartScreen.jsx
+++ src/screens/CartScreen.jsx
@@ -8,10 +8,6 @@ import returnCart from "../helper/CartList";
 
 
 // const cpi= 200;
-const orderCount = 2;
-const itemCost = 200;
-const orderID = 123456;
-const FinalTotal = 0
 // const Productvalue = new Array();
 // const ProductCount = new Array();
 const CalculateTotal = (ProductArray)=>{
@@ -28,8 +24,20 @@ const CalculateTotal = (ProductArray)=>{
     return Total;
 }
 
-
-
+const product = [{
+    bookname : "Harry Potter",
+    img : "https://images-na.ssl-images-amazon.com/images/I/51ZuFk7r86L._SX331_BO1,204,203,200_.jpg",
+    orderid : 123456,
+    tag : {
+        sellerid : "Seller1",
+        address : "Delhi",
+        price : 200,
+        offered_price : 150,
+        offer : true,
+        count : 2,
+        delivery : true
+    }
+}];
 
 
 const Cart = () => {   
@@ -71,51 +79,20 @@ const Cart = () => {
                 </Container>
                 </div>
                 }
-                {/* Dynamically add any number of items */}
                 </div>
-
                 {/* Order History Section */}
                 <div className="order-history">
                     <header className="heading1">
                     Order History
                     </header>
                     <hr />
-
-                {/* Add condition for no order history */}
-                {/* If no items, show this */}
-                    <div className="no-items">You have no order history!</div>
-
                 
-                    {/* Dynamically add any number of items */}
-                    <Card style={{height: "175px", margin: "20px 30px"}}>
-                    <Container>       
-                        <Row className="justify-content-start align-items-center">
-                            <Col sm={2}>
-                                <Card.Img variant="left" style={{height: "150px", width:"150px", marginTop:"12.5px", marginLeft: "12.5px"}} />
-                            </Col>
-                            
-                            <Col sm={6}>
-                                <Card.Title>Product Name which may be very long</Card.Title>
-                                <Card.Subtitle>Seller Name, Location</Card.Subtitle>
-                                <div className="description">Product Description, things like tags</div>
-                            </Col>
-
-                            <Col sm={2}>
-                                {orderCount}{"  *   "}<i className="fa fa-inr"></i>{" "}{itemCost}{" = "}<i className="fa fa-inr"></i>{" "}{orderCount*itemCost}
-                            </Col>
-
-                            <Col sm={2}>
-                                {/* Status of Order */}
-                                <div className="text-center">
-                                    {/* Delivered, Processing, Out for Delivery */}
-                                    <b>Delivered</b> <br />
-                                    <text><b>ORDER ID:</b> {orderID}</text>
-                                </div>
-                            </Col>
-
-                        </Row>
-                    </Container>
-                    </Card>
+                {product === null || product.length===0?
+                    <div className="no-items">You have no order history!</div>:
+                product.map((book)=>{
+                    return <HistoryCard product={book}/>
+                })}
+                    
                 </div>
             </div>
         )
@@ -168,6 +145,39 @@ const CartCard = ({product})=>{
                     </Container>
                 </Card>
     )
+}
+
+const HistoryCard = ({product})=>{
+   return( <Card style={{height: "175px", margin: "20px 30px"}}>
+                    <Container>       
+                        <Row className="justify-content-start align-items-center">
+                            <Col sm={2}>
+                                <Card.Img variant="left" style={{height: "150px", width:"150px", marginTop:"12.5px", marginLeft: "12.5px"}} />
+                            </Col>
+                            
+                            <Col sm={6}>
+                                <Card.Title>{product.name}</Card.Title>
+                                <Card.Subtitle>{product.tag.sellerid}</Card.Subtitle>
+                                <div className="description"> {product.tag.address}</div>
+                            </Col>
+
+                            <Col sm={2}>
+                                <i className="fa fa-inr"></i>
+                                {product.tag.offer?product.tag.offered_price*product.tag.count:product.tag.price*product.tag.count}
+                            </Col>
+
+                            <Col sm={2}>
+                                {/* Status of Order */}
+                                <div className="text-center">
+                                    {/* Delivered, Processing, Out for Delivery */}
+                                    <b>{product.tag.delivery?"Delivered":"Pending"}</b> <br />
+                                    <text><b>ORDER ID:</b> {product.orderid}</text>
+                                </div>
+                            </Col>
+
+                        </Row>
+                    </Container>
+    </Card>)
 }
 
 export default Cart
