5c639563e355160488e5194b16155c9827b05431
Aarya <aaryashelar000@gmail.com>
2021-11-11 23:58:05 +0530

    pricing page final

--- src/screens/PricingScreen.jsx
+++ src/screens/PricingScreen.jsx
@@ -21,7 +21,7 @@ const PricingScreen = () => {
             <Container fluid>
                 <Row>
                     <Col className="card-col">
-                        <Card border="dark" style={{height: "auto", margin: "40px 12px 20px 12px", padding: "25px 18px"}}>
+                        <Card border="dark" style={{height: "450px", margin: "40px 12px 20px 12px", padding: "25px 18px 10px 18px"}}>
                             <Card.Title align="center" className="plan-name">BRONZE PLAN 🥉</Card.Title>
 
                             <br />
@@ -29,74 +29,74 @@ const PricingScreen = () => {
                             <Card.Text className="perks">
                                 <h5>Perks of the plan</h5>
                                 <ul>
-                                    <li> 1 month membership</li>
-                                    <li> Get 5 days free</li>
-                                    <li> ₹100</li>
+                                    <li style={{color: "black"}}> <b>1 month</b> membership</li>
+                                    <li style={{color: "black"}}> Get <b>5 days free</b></li>
                                 </ul>
                             </Card.Text>
                             <br />
-                            <Card.Text align="center">
+                            <Card.Text align="center" style={{marginTop: "101px"}}>
                                 <h3><i className="fa fa-inr"></i><span style={{marginLeft: "8px"}}>100 /-</span></h3>
                             </Card.Text>
                         </Card>
                     </Col>
 
                     <Col className="card-col">
-                        <Card border="dark" style={{height: "auto", margin: "40px 12px 20px 12px", padding: "25px 18px"}}>
+                        <Card border="dark" style={{height: "450px", margin: "40px 12px 20px 12px", padding: "25px 18px 10px 18px"}}>
                             <Card.Title align="center" className="plan-name">SILVER PLAN 🥈</Card.Title>
                             <br />
                             <hr width="85%" style={{ margin:"0px 7% 25px 7%" }} />
                             <Card.Text className="perks">
                                 <h5>Perks of the plan</h5>
                                 <ul>
-                                    <li>3 months membership</li>
-                                    <li>Get 15 days free</li>
-                                    <li>₹270</li>
+                                    <li style={{color: "black"}}> <b>3 months</b> membership</li>
+                                    <li style={{color: "black"}}> Get <b>15 days free</b></li>
                                 </ul>
                             </Card.Text>
                             <br />
-                            <Card.Text align="center">
+                            <Card.Text align="center" style={{marginTop: "100px"}}>
                                 <h3><i className="fa fa-inr"></i><span style={{marginLeft: "8px"}}>270 /-</span></h3>
                             </Card.Text>
                         </Card>
                     </Col>
 
                     <Col className="card-col">
-                        <Card border="dark" style={{height: "auto", margin: "40px 12px 20px 12px", padding: "25px 18px"}}>
+                        <Card border="dark" style={{height: "450px", margin: "40px 12px 20px 12px", padding: "25px 18px 10px 18px"}}>
                             <Card.Title align="center" className="plan-name">GOLD PLAN 🥇</Card.Title>
                             <br />
                             <hr width="85%" style={{ margin:"0px 7% 25px 7%" }} />
                             <Card.Text className="perks">
                                 <h5>Perks of the plan</h5>
                                 <ul>
-                                    <li> 6 month membership</li>
-                                    <li> Get 30 days free</li>
-                                    <li> ₹550</li>
+                                    <li style={{color: "black"}}> <b>6 months</b> membership</li>
+                                    <li style={{color: "black"}}> Get <b>30 days free</b></li>
+                                    <li> <b>Sales & Advertising Priority</b></li>
+                                    <li> <b>Priority Problem Solving</b></li>
                                 </ul>
                             </Card.Text>
                             <br />
-                            <Card.Text align="center">
+                            <Card.Text align="center" style={{marginTop: "34px"}}>
                                 <h3><i className="fa fa-inr"></i><span style={{marginLeft: "8px"}}>550 /-</span></h3>
                             </Card.Text>
                         </Card>
                     </Col>
 
                     <Col className="card-col">
-                        <Card border="dark" style={{height: "auto", margin: "40px 12px 20px 12px", padding: "25px 18px"}}>
+                        <Card border="dark" style={{height: "450px", margin: "40px 12px 20px 12px", padding: "25px 18px 10px 18px"}}>
                             <Card.Title align="center" className="plan-name">PLATINUM PLAN 💎</Card.Title>
                             <br />
                             <hr width="85%" style={{ margin:"0px 7% 25px 7%" }} />
                             <Card.Text className="perks">
-                                <h6>Perks of the plan</h6>
+                                <h5>Perks of the plan</h5>
                                 <ul>
-                                    <li> 1 year membership</li>
-                                    <li> Get 60 days free</li>
-                                    <li> ₹1000</li>
-                                    
+                                    <li style={{color: "black"}}> <b>1 year</b> membership</li>
+                                    <li style={{color: "black"}}> Get <b>60 days free</b></li>
+                                    <li> <b>Sales/Advertising Priority</b></li>
+                                    <li> <b>Priority Problem Solving</b></li>
+                                    <li> <b>Access to future events/conferences</b></li>
                                 </ul>
                             </Card.Text>
                             <br />
-                            <Card.Text align="center">
+                            <Card.Text align="center" style={{marginTop: "0px"}}>
                                 <h3><i className="fa fa-inr"></i><span style={{marginLeft: "8px"}}>1000 /-</span></h3>
                             </Card.Text>
                         </Card>
